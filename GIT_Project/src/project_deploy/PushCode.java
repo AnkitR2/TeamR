baf5bf820d8f4e616b930f1b76c61527c1ca76f8
AnkitR2 <ankitrasane2@gmail.com>
2021-09-22 22:03:47 +0530

    New Change

--- GIT_Project/src/project_deploy/PushCode.java
+++ GIT_Project/src/project_deploy/PushCode.java
@@ -3,5 +3,5 @@ package project_deploy;
 public class PushCode {
 public static void main(String[] args) {
 	System.out.println("This is GIT Project Start");
-}
+	}
 }
---
+++ GIT_Project/src/project_deploy/TestClass.java
@@ -0,0 +1,16 @@
+package project_deploy;
+
+public class TestClass {
+static int i;
+static int j;
+	public static void main(String[] args) {
+		for(i=0;i<=5;i++) {
+			for(j=0;j<=i ;j++) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+		System.out.println("This is Test Class");
+		
+	}
+}
